46664a120c2102c3339759b9db5371edaf2f4fbf
OrtizBrayan <brayanortizbastidas@gmail.com>
2022-06-07 01:12:06 -0500

    processing algorithm of floyd warshall

--- app/src/main/java/com/project/graphpuzzleapp/MainActivity.kt
+++ app/src/main/java/com/project/graphpuzzleapp/MainActivity.kt
@@ -7,5 +7,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+    }
+
+    fun exampleInstance (){
+        var arreglo: Array<Array<Int>> = arrayOf(arrayOf(1,2,3), arrayOf(1,2,3), arrayOf(1,2,3))
+        var road: Array<Array<Int>> = arrayOf(arrayOf(1,2,3), arrayOf(1,2,3), arrayOf(1,2,3))
+
+        val values: Roads = Roads(arreglo,road)
+
+        values.mapRoads()
     }
 }
