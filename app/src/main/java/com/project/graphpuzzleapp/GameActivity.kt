3093a4255cc9cb17fffc0e1f4c9537f74a49be2e
OrtizBrayan <brayanortizbastidas@gmail.com>
2022-07-02 00:05:11 -0500

    Added uppercase text filter format to EditText of vertexes

--- app/src/main/java/com/project/graphpuzzleapp/GameActivity.kt
+++ app/src/main/java/com/project/graphpuzzleapp/GameActivity.kt
@@ -1,17 +1,18 @@
 package com.project.graphpuzzleapp
 
-import android.graphics.Canvas
-import androidx.appcompat.app.AppCompatActivity
+import android.R
 import android.os.Bundle
-import android.text.Editable
+import android.text.InputFilter
+import android.text.InputFilter.AllCaps
 import android.view.View
-import android.widget.Toast
-import androidx.lifecycle.ViewModel
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.project.graphpuzzleapp.databinding.ActivityGameBinding
 import com.project.model.Game
 import com.project.model.GraphGame
 import com.project.model.Player
+
 
 class GameActivity : AppCompatActivity() {
 
@@ -26,8 +27,27 @@ class GameActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        initEditTextFormat()
         newGame()
         nextGame()
+
+    }
+
+    private fun initEditTextFormat(){ //Uppercase text filter formatting
+        val vertex1 = binding.vertex1 as EditText
+        vertex1.filters = arrayOf<InputFilter>(AllCaps())
+
+        val vertex2 = binding.vertex2 as EditText
+        vertex2.filters = arrayOf<InputFilter>(AllCaps())
+
+        val vertex3 = binding.vertex3 as EditText
+        vertex3.filters = arrayOf<InputFilter>(AllCaps())
+
+        val vertex4 = binding.vertex4 as EditText
+        vertex4.filters = arrayOf<InputFilter>(AllCaps())
+
+        val vertex5 = binding.vertex5 as EditText
+        vertex5.filters = arrayOf<InputFilter>(AllCaps())
     }
 
     private fun nextGame(){
